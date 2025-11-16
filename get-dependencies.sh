789ac525fef09a832b7b9ceabf221f4df31398b1
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-16 10:22:47 +0100

    Migrate to new generic AppImage template

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,32 +1,13 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-ARCH="$(uname -m)"
-DEBLOATED_PKGS_INSTALLER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+ARCH=$(uname -m)
 
-echo "Installing build dependencies for sharun & AppImage integration..."
+echo "Installing package and its dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	base-devel \
-	curl \
-	desktop-file-utils \
-	git \
-	libxtst \
-	wget \
-	xorg-server-xvfb \
-	zsync
-echo "Installing the app & it's dependencies..."
-echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	warp
+pacman -Syu --noconfirm warp
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS_INSTALLER" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh libxml2-mini mesa-nano gtk4-mini gdk-pixbuf2-mini librsvg-mini opus-mini
-
-echo "Extracting the app version into a version file"
-echo "---------------------------------------------------------------"
-pacman -Q warp | awk '{print $2; exit}' > ~/version
+get-debloated-pkgs --add-common --prefer-nano
--- make-appimage.sh
+++ make-appimage.sh
@@ -1,24 +1,21 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-ARCH="$(uname -m)"
-VERSION="$(cat ~/version)"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-
-# Variables used by quick-sharun
+ARCH=$(uname -m)
+VERSION=$(pacman -Q warp | awk '{print $2; exit}')
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export OUTNAME=Warp-"$VERSION"-anylinux-"$ARCH".AppImage
-export DESKTOP=/usr/share/applications/app.drey.Warp.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/app.drey.Warp.svg
+export DESKTOP=/usr/share/applications/app.drey.Warp.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1 # For camera portal
 export STARTUPWMCLASS=warp # For Wayland, this is 'app.drey.Warp', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
-wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun
-./quick-sharun /usr/bin/warp
+quick-sharun /usr/bin/warp
 
 ## Copy help files for Help section to work
 langs=$(find /usr/share/help/*/warp/ -type f | awk -F'/' '{print $5}' | sort | uniq)
@@ -27,10 +24,5 @@ for lang in $langs; do
   cp -vr /usr/share/help/$lang/warp/* ./AppDir/share/help/$lang/warp/
 done
 
-# Make the AppImage with uruntime
-./quick-sharun --make-appimage
-
-# Prepare the AppImage for release
-mkdir -p ./dist
-mv -v ./*.AppImage* ./dist
-mv -v ~/version     ./dist
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
