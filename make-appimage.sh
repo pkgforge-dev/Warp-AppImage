5184dcde2c9ee0dd00afeb457d6cd364cd0eb080
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-16 10:33:45 +0100

    Don't add self-updater hook

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,6 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q warp | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/app.drey.Warp.svg
 export DESKTOP=/usr/share/applications/app.drey.Warp.desktop
