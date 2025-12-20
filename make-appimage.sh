aa7b60d2e80f00f4d332cf148408859ae8d92adb
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-20 19:34:38 +0100

    OpenGL is automatically deployed now

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,6 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/app.drey.Warp.svg
 export DESKTOP=/usr/share/applications/app.drey.Warp.desktop
-export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1 # For camera portal
 export STARTUPWMCLASS=app.drey.Warp # Default to Wayland's wmclass. For X11, GTK_CLASS_FIX will force the wmclass to be the Wayland one.
 export GTK_CLASS_FIX=1
